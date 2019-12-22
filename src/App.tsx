1315a5bdd06e5f1c401c8a43b67c8e77ee673bf7
motionrus <tytin@yandex.ru>
2019-12-22 08:29:29 +0300

    basic auth
    ** installed and configured eslint
    ** installed router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React from "react"
+import Header from "./components/Header/Header"
+import {ThemeProvider} from "@material-ui/core/styles"
+import "antd/dist/antd.css"
+import {blue} from "@material-ui/core/colors"
+import {createMuiTheme} from "@material-ui/core/styles"
+import SignIn from "./components/SignIn/SignIn"
+import {BrowserRouter as Router, Route} from "react-router-dom"
+import SignUp from "./components/SignUp/SignUp"
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {main: blue[500]}, // Purple and green play nicely together.
+    secondary: {main: "#11cb5f"}, // This is just green.A700 as hex.
+  },
+})
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
-  );
+    <ThemeProvider theme={theme}>
+      <Router >
+        <Header auth={false}/>
+        <Route path="/sign-in" component={SignIn}/>
+        <Route path="/sign-up" component={SignUp}/>
+      </Router>
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,111 @@
+import React from "react"
+import {createStyles, makeStyles, Theme} from "@material-ui/core/styles"
+import AppBar from "@material-ui/core/AppBar"
+import Toolbar from "@material-ui/core/Toolbar"
+import Typography from "@material-ui/core/Typography"
+import IconButton from "@material-ui/core/IconButton"
+import MenuIcon from "@material-ui/icons/Menu"
+import AccountCircle from "@material-ui/icons/AccountCircle"
+import MenuItem from "@material-ui/core/MenuItem"
+import Menu from "@material-ui/core/Menu"
+import {Button} from "@material-ui/core"
+import {useHistory} from "react-router-dom"
+
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+      color: "#FFF"
+    },
+    white: {
+      color: "#FFF",
+      "&:hover": {
+        color: "#FFF",
+      }
+    }
+  }),
+)
+
+interface MenuAppBarProps {
+  auth: boolean
+}
+
+const MenuAppBar: React.FC<MenuAppBarProps> = ({auth}) => {
+  const classes = useStyles()
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
+  const open = Boolean(anchorEl)
+  const history = useHistory()
+
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon/>
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Simple Chat
+          </Typography>
+          {auth && (
+            <div>
+              <IconButton
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <AccountCircle/>
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={open}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+              </Menu>
+            </div>
+          )}
+          {!auth && (
+            <React.Fragment>
+              <Button className={classes.white} onClick={() => (history.push("/sign-in"))}>
+                SignIn
+              </Button>
+              <Button className={classes.white} onClick={() => (history.push("/sign-up"))}>
+                SignUp
+              </Button>
+            </React.Fragment>
+          )}
+        </Toolbar>
+      </AppBar>
+    </div>
+  )
+}
+
+export default MenuAppBar
